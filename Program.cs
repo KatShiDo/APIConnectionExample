ef278038b744ac08712fbf600d021c430f760a9b
Nikita <poberezhnyuk.n.i@edu.mirea.ru>
2023-09-10 13:06:49 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace APIConnectionExample
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            ApiHelper.InitializeClient();
+
+            var sunInfo = await SunProcessor.LoadSunInfo(DateTime.Now);
+            Console.WriteLine($"Sunrise in Moscow today is at {sunInfo.Sunrise.ToLocalTime().ToShortTimeString()}");
+            Console.WriteLine($"Sunset in Moscow today is at {sunInfo.Sunset.ToLocalTime().ToShortTimeString()}\n");
+
+
+            sunInfo = await SunProcessor.LoadSunInfo(DateTime.Parse("2022-06-14"));
+            Console.WriteLine($"The earliest sunrise in Moscow was at {sunInfo.Sunrise.ToLocalTime().ToShortTimeString()}\n");
+
+
+            sunInfo = await SunProcessor.LoadSunInfo(DateTime.Parse("2022-06-25"));
+            Console.WriteLine($"The latest sunset in Moscow was at {sunInfo.Sunset.ToLocalTime().ToShortTimeString()}\n");
+        }
+    }
+}
---
+++ SunProcessor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIConnectionExample
+{
+    public class SunProcessor
+    {
+        public static async Task<SunModel> LoadSunInfo(DateTime date)
+        {
+            string url = $"https://api.sunrise-sunset.org/json?lat=55.45&lng=37.36&date={date}";
+
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(url))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    SunResultModel result = await response.Content.ReadAsAsync<SunResultModel>();
+
+                    return result.Results;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
